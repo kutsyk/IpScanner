ddd50523a3233d8adf4cbd24cb39db1c0406f835
Vasyl Kutsyk <Vasyl Kutsyk>
2016-11-14 23:13:53 +0200

    amazing tables

--- scanner.py
+++ scanner.py
@@ -15,7 +15,6 @@ ERRORS_DATABASE = 'dbs/errors'
 
 available_threads = 8
 ip_queue = Queue()
-OWNER_NAME = ''
 # Read data from Azure table
 
 CLIENT = document_client.DocumentClient(DOCUMENTDB_HOST, {'masterKey': DOCUMENTDB_KEY})
@@ -29,44 +28,70 @@ table_service = TableService(account_name='ipstats',
 
 nm = nmap.PortScanner()
 
+
 def scan(i, q):
     host = q.get()
     # TODO: geolocation script --script ip-geolocation-geoplugin
     nm.scan(host, arguments="-O -A")
     if host in nm.all_hosts():
         CLIENT.CreateDocument(banners['_self'], {
-            'id': OWNER_NAME + '_id_' + host,
+            'id': host.PartitionKey + '_id_' + host,
             'info': nm[host]
         })
     q.task_done()
 
-def scnner_function(i, q):
-    print "Thread %d: started" % i
-    while True:
-        scan(i, q)    
+
+# def scnner_function(i, q):
+#     print "Thread %d: started" % i
+#     while True:
+        # scan(i, q)
+
 
 def main():
-    owners = table_service.query_entities('owners')
-	
-    workers = []
-    for i in xrange(available_threads):
-        worker = Thread(target=scnner_function, args=(i, ip_queue))
-        worker.setDaemon(True)
-        workers.append(worker)
+    pagination = 10
+    # owners = table_service.query_entities('owners')
+    # print len(owners)
+    # workers = []
+    # for i in xrange(available_threads):
+    #     worker = Thread(target=scnner_function, args=(i, ip_queue))
+    #     worker.setDaemon(True)
+    #     workers.append(worker)
 
-    for owner in owners:
-            ipAddresses = table_service.query_entities('ipAddress', filter="PartitionKey eq '" + owner.Name + "'")
+    # for owner in owners:
+    counter = 0
+    ipAddresses = table_service.query_entities('ipAddress', filter='', top=1)
+    nextPartKey = ipAddresses.x_ms_continuation['NextPartitionKey']
+    nextRowKey = ipAddresses.x_ms_continuation['NextRowKey']
+    currentIp = ipAddresses.pop()
 
-            for ip in ipAddresses:
-                ip_queue.put(ip.Address)
-            global OWNER_NAME
-            OWNER_NAME = owner.Name
-            
-            for worker in workers:
-                worker.start()
+    while True:
+        oneObjectList = table_service.query_entities('ipAddress', filter='', top=1, next_partition_key=nextPartKey, next_row_key=nextRowKey)
+        print currentIp.Address
+        currentIp = oneObjectList.pop()
+        nextPartKey = oneObjectList.x_ms_continuation['NextPartitionKey']
+        nextRowKey = oneObjectList.x_ms_continuation['NextRowKey']
+        counter += 1
+        if counter == pagination:
+            break
 
-            ip_queue.join()
-            ip_queue.queue.clear()
+    # print second.x_ms_continuation['NextRowKey']
+    # print second.x_ms_continuation['NextPartitionKey']
+    # for ip in second:
+    #     print ip.PartitionKey
+        # print ip.RowKey
+
+    #     ip_queue.put(ip.Address)
+
+    # global OWNER_NAME
+    # OWNER_NAME = owner.Name
+
+    # print OWNER_NAME
+    # for worker in workers:
+    #     worker.start()
+
+    # ip_queue.join()
+    # ip_queue.queue.clear()
+
 
 if __name__ == '__main__':
     main()
