485846fd8eb802205f9561303e86c761698c6702
Vasyl Kutsyk <Vasyl Kutsyk>
2016-11-15 22:27:46 +0200

    Code for country, commit for victory!

--- scanner.py
+++ scanner.py
@@ -1,101 +1,72 @@
 #!/usr/bin/python
 from threading import Thread
-import threading 
+import threading
 import nmap
-from Queue import Queue
-from azure.storage.table import TableService, Entity
+from azure.storage.table import TableService
 import pydocumentdb.document_client as document_client
-from time import gmtime, strftime
 
-#DOCUMENTDB_HOST = 'https://ipstats.documents.azure.com:443/'
-#DOCUMENTDB_KEY = 'FuRTjt01UVmWS1KRPxkbLxOw7imKhNyHIWluSxZ8rjwZrJSZwJKJUNBYhAzDsiOHk2yKdzv9JhQOuEHWtDhZ4w=='
+DOCUMENTDB_HOST = 'https://ipstats.documents.azure.com:443/'
+DOCUMENTDB_KEY = 'FuRTjt01UVmWS1KRPxkbLxOw7imKhNyHIWluSxZ8rjwZrJSZwJKJUNBYhAzDsiOHk2yKdzv9JhQOuEHWtDhZ4w=='
 
-#DOCUMENTDB_COLLECTION = 'host-banners'
-#DOCUMENTDB_DATABASE = 'dbs/host-banners'
-#ERRORS_DATABASE = 'dbs/errors'
+DOCUMENTDB_COLLECTION = 'host-banners'
+DOCUMENTDB_DATABASE = 'dbs/host-banners'
 
 available_threads = 8
-ip_queue = Queue()
 
-#CLIENT = document_client.DocumentClient(DOCUMENTDB_HOST, {'masterKey': DOCUMENTDB_KEY})
-#DB = list(CLIENT.QueryDatabases("SELECT * FROM root r WHERE r.id='host-banners'"))
-#document_coll = list(CLIENT.ReadCollections(DOCUMENTDB_DATABASE))
-
-#banners = document_coll[0]
+CLIENT = document_client.DocumentClient(DOCUMENTDB_HOST, {'masterKey': DOCUMENTDB_KEY})
+DB = list(CLIENT.QueryDatabases("SELECT * FROM root r WHERE r.id='host-banners'"))
+DOCUMENTS_COLL = list(CLIENT.ReadCollections(DOCUMENTDB_DATABASE))
+banners = DOCUMENTS_COLL[0]
 
 table_service = TableService(account_name='ipstats',
                              account_key='yjtopnZUk0TvdrNixtWUGcyt0FJuUwolOFFLiwpUtFWHBSt9L4i/AsBWo4Hnpsd+Thf5xNCKczntE4MOM3XqRA==')
 
+nextPartKey = None
+nextRowKey = None
+
 nm = nmap.PortScanner()
 
-def scan(i, q):
-    host = q.get()
+
+def scan(host):
     # TODO: geolocation script --script ip-geolocation-geoplugin
-    nm.scan(host, arguments="-O -A")
+    nm.scan(host.Address, arguments="-O -A")
     if host in nm.all_hosts():
         CLIENT.CreateDocument(banners['_self'], {
-            'id': host.PartitionKey + '_id_' + host,
-            'info': nm[host]
+            'id': host.PartitionKey + '_id_' + host.Address,
+            'info': nm[host.Address]
         })
-    q.task_done()
 
 
 def scnner_function(i, lock):
-     with lock:
-         oneObjectList = table_service.query_entities('ipAddress', filter='', top=1, next_partition_key=nextPartKey, next_row_key=nextRowKey)
-         currentIp = oneObjectList.pop()
-         global nextPartKey
-         nextPartKey = oneObjectList.x_ms_continuation['NextPartitionKey']
-         global nextRowKey
-         nextRowKey = oneObjectList.x_ms_continuation['NextRowKey']
-         print currentIp.Address
-#     print "Thread %d: started" % i
-#     while True:
-        # scan(i, q)
-
-
-def main():    
-    counter = 0
-    ipAddresses = table_service.query_entities('ipAddress', filter='', top=1)
+    print "Thread ", i
     global nextPartKey
-    nextPartKey = ipAddresses.x_ms_continuation['NextPartitionKey']
     global nextRowKey
-    nextRowKey = ipAddresses.x_ms_continuation['NextRowKey']
-    currentIp = ipAddresses.pop()
-    print currentIp.Address
-    
+
+    while True:
+        with lock:
+            oneObjectList = table_service.query_entities('ipAddress', filter='', top=1, next_partition_key=nextPartKey,
+                                                         next_row_key=nextRowKey)
+            currentIp = oneObjectList.pop()
+            nextPartKey = oneObjectList.x_ms_continuation['NextPartitionKey']
+            nextRowKey = oneObjectList.x_ms_continuation['NextRowKey']
+            if not nextPartKey and not nextRowKey:
+                break
+        scan(currentIp)
+
+def main():
     workers = []
     lock = threading.Lock()
     for i in xrange(available_threads):
-         worker = Thread(target=scnner_function, args=(i, lock))
-         worker.setDaemon(True)
-         workers.append(worker)
-         
+        worker = Thread(target=scnner_function, args=(i, lock))
+        worker.setDaemon(True)
+        workers.append(worker)
+
     for worker in workers:
         worker.start()
-    #while nextPartKey and nextRowKey:
-        #oneObjectList = table_service.query_entities('ipAddress', filter='', top=1, next_partition_key=nextPartKey, next_row_key=nextRowKey)
-        #currentIp = oneObjectList.pop()
-        #nextPartKey = oneObjectList.x_ms_continuation['NextPartitionKey']
-        #nextRowKey = oneObjectList.x_ms_continuation['NextRowKey']
-        #counter += 1
-        #if (counter % 10000 == 0):
-        #print counter
 
-        
-    print counter
-    
-    #     ip_queue.put(ip.Address)
-    # OWNER_NAME = owner.Name
-
-    # print OWNER_NAME
-    # for worker in workers:
-    #     worker.start()
-
-    # ip_queue.join()
-    # ip_queue.queue.clear()
     for worker in workers:
         worker.join()
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 import nmap
+import pydocumentdb.document_client as document_client
+
+DOCUMENTDB_HOST = 'https://ipstats.documents.azure.com:443/'
+DOCUMENTDB_KEY = 'FuRTjt01UVmWS1KRPxkbLxOw7imKhNyHIWluSxZ8rjwZrJSZwJKJUNBYhAzDsiOHk2yKdzv9JhQOuEHWtDhZ4w=='
+
+DOCUMENTDB_COLLECTION = 'host-banners'
+DOCUMENTDB_DATABASE = 'dbs/host-banners'
+
+CLIENT = document_client.DocumentClient(DOCUMENTDB_HOST, {'masterKey': DOCUMENTDB_KEY})
+DB = list(CLIENT.QueryDatabases("SELECT * FROM root r WHERE r.id='host-banners'"))
+DOCUMENTS_COLL = list(CLIENT.ReadCollections(DOCUMENTDB_DATABASE))
+banners = DOCUMENTS_COLL[0]
 
 nm = nmap.PortScanner()
 
 host = '185.15.211.114'
-nm.scan(host)
+nm.scan(host, arguments="-O -A")
 if nm[host]:
-    print nm[host]
+    CLIENT.CreateDocument(banners['_self'], {
+        'id': 'test_185.15.211.114',
+        'info': nm[host]
+    })
+    print 'Success'
+else:
+    print 'Fail'
