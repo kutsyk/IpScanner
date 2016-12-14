968826dd3342b15338bd5a167eb23fcc1f249480
kutsyk <vasya.kutsyk@gmail.com>
2016-12-14 21:59:21 +0200

    removed unused import

--- Back/cloud.py
+++ Back/cloud.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python
-import sys
-import pydocumentdb.document_client as document_client
-import ConfigParser
-import pymongo
 from pymongo.mongo_client import MongoClient
 
-# class Cloud:
-#     configParser = ConfigParser.RawConfigParser()
-#     configFilePath = r'config.txt'
-#     configParser.read(configFilePath)
-#
-#     DOCUMENTDB_HOST = configParser.get('cloud-config', 'DOCUMENTDB_HOST')
-#     DOCUMENTDB_KEY = configParser.get('cloud-config', 'DOCUMENTDB_KEY')
-#
-#     DOCUMENTDB_COLLECTION = configParser.get('cloud-config', 'DOCUMENTDB_COLLECTION')
-#     DOCUMENTDB_DATABASE = configParser.get('cloud-config', 'DOCUMENTDB_DATABASE')
-#
-#     CLIENT = document_client.DocumentClient(DOCUMENTDB_HOST, {'masterKey': DOCUMENTDB_KEY})
-#     DB = list(CLIENT.QueryDatabases("SELECT * FROM root r WHERE r.id='host-banners'"))
-#     DOCUMENTS_COLL = list(CLIENT.ReadCollections(DOCUMENTDB_DATABASE))
-#     banners = DOCUMENTS_COLL[0]
-#     banners_dev = DOCUMENTS_COLL[1]
 
 MONGODB_URI = "mongodb://user:pass@ds133348.mlab.com:33348/ipstats"
 class MongoConnector:
