38fc3274e5f2e0f1efbb120ad5925a61f9a387fa
kutsyk <vasya.kutsyk@gmail.com>
2016-12-18 00:48:42 +0200

    try it

--- Back/scanner.py
+++ Back/scanner.py
@@ -19,48 +19,51 @@ ipNetworksQueue = Queue()
 nm = nmap.PortScanner()
 args = "--min-rate 1000 --max-retries 0 -sV -Pn --script=http-title --script=http-headers"
 
+
 def GetPlainString(string):
     return string.replace('\n', ' ').replace('\r', '')
+
 
 def scan(conn, host, nm, icmp):
     # TODO: geolocation script --script ip-geolocation-geoplugin
     pack = IP(dst=host) / icmp
-    reply = sr1(pack, timeout=TIMEOUT, verbose=False)
-    if reply is not None:
-        try:
-            nm.scan(host, arguments=args)
-            if host in nm.all_hosts():
-                if nm[host].state() == 'up':
-                    res = {}
-                    res["tcp"] = []
-                    lport = nm[host]["tcp"].keys()
-                    for port in lport:
-                        portInfo = {"port": port}
-                        for portKey in nm[host]["tcp"][port].keys():
-                            if portKey == 'script':
-                                portInfo[portKey] = {}
-                                for httpKeys in nm[host]["tcp"][port][portKey].keys():
-                                    portInfo[portKey][httpKeys] = GetPlainString(
-                                        nm[host]["tcp"][port][portKey][httpKeys])
-                            else:
-                                portInfo[portKey] = GetPlainString(nm[host]["tcp"][port][portKey])
-                        res["tcp"].append(portInfo)
+    try:
+        reply = sr1(pack, timeout=TIMEOUT, verbose=False)
+        if reply is not None:
+            try:
+                nm.scan(host, arguments=args)
+                if host in nm.all_hosts():
+                    if nm[host].state() == 'up':
+                        res = {}
+                        res["tcp"] = []
+                        lport = nm[host]["tcp"].keys()
+                        for port in lport:
+                            portInfo = {"port": port}
+                            for portKey in nm[host]["tcp"][port].keys():
+                                if portKey == 'script':
+                                    portInfo[portKey] = {}
+                                    for httpKeys in nm[host]["tcp"][port][portKey].keys():
+                                        portInfo[portKey][httpKeys] = GetPlainString(
+                                            nm[host]["tcp"][port][portKey][httpKeys])
+                                else:
+                                    portInfo[portKey] = GetPlainString(nm[host]["tcp"][port][portKey])
+                            res["tcp"].append(portInfo)
 
-                    res["hostnames"] = nm[host]["hostnames"]
-                    res["addresses"] = nm[host]["addresses"]
-                    res["vendor"] = nm[host]["vendor"]
+                        res["hostnames"] = nm[host]["hostnames"]
+                        res["addresses"] = nm[host]["addresses"]
+                        res["vendor"] = nm[host]["vendor"]
 
-                    conn.ipsBanners.insert(res)
-        except AttributeError as e:
-            print str(e)
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
-            gc.collect()
-
+                        conn.ipsBanners.insert(res)
+            except AttributeError as e:
+                print str(e)
+            except:
+                print "Unexpected error:", sys.exc_info()[0]
+                gc.collect()
+    except:
+        print "Ping error:", sys.exc_info()[0]
+        gc.collect()
     del pack
-    del reply
-
-# def createProperBsonObject(obj)
+    del reply  # def createProperBsonObject(obj)
 
 def scanner_function(i, q):
     print "Thread ", i
