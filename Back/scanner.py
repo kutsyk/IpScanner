038b41ded22c286f986ebede443e1574f97a2941
kutsyk <vasya.kutsyk@gmail.com>
2017-01-18 20:55:47 +0200

    script sctructure update

--- Back/scanner.py
+++ Back/scanner.py
@@ -26,7 +26,7 @@ def GetPlainString(string):
 
 def scan(conn, host, nm, icmp):
     # TODO: geolocation script --script ip-geolocation-geoplugin   
-    pack = IP(dst=host)/icmp
+    pack = IP(dst=host) / icmp
     conn.processedIps.insert({"_id": int(netaddr.IPAddress(host))})
     try:
         reply = sr1(pack, timeout=TIMEOUT, verbose=False)
@@ -35,8 +35,8 @@ def scan(conn, host, nm, icmp):
                 nm.scan(host, arguments=args)
                 if host in nm.all_hosts():
                     if nm[host].state() == 'up':
-                        jsonRes = json.dumps(nm[host])                
-                        conn.ipsBanners.insert({ "info" : jsonRes })
+                        jsonRes = json.dumps(nm[host])
+                        conn.ipsBanners.insert({"info": jsonRes})
             except KeyError as e:
                 print str(e)
             except ValueError as e:
@@ -53,19 +53,19 @@ def scan(conn, host, nm, icmp):
     except:
         print "Ping error:", sys.exc_info()[0]
         gc.collect()
-        
+
     del pack
-    
+
 
 def scanner_function(i, q):
     print "Thread ", i
     icmp = ICMP()
     threadConn = MongoConnector()
-    while True:
+    while not q.empty():
         network = q.get()
         ipNet = IPNetwork(network)
         for ip in ipNet:
-            if (threadConn.processedIps.count({"_id" : int(netaddr.IPAddress(ip))}) == 0):
+            if (threadConn.processedIps.count({"_id": int(netaddr.IPAddress(ip))}) == 0):
                 scan(threadConn, str(ip), nm, icmp)
 
         q.task_done()
@@ -73,23 +73,22 @@ def scanner_function(i, q):
         gc.collect()
 
 
-def main():
-    print "Program started"
-    with open('CIDR.txt', 'r') as cidr_file:
-        line = cidr_file.readlines()
-        for l in line:
-            if l.startswith("#"):
-                continue
-            ipNetworksQueue.put(l)
+    # def main():
+print "Program started"
+with open('CIDR.txt', 'r') as cidr_file:
+    line = cidr_file.readlines()
+    for l in line:
+        if l.startswith("#"):
+            continue
+        ipNetworksQueue.put(l)
 
-    for i in xrange(AVAILABLE_THREADS):
-        worker = Thread(target=scanner_function, args=(i, ipNetworksQueue))
-        worker.setDaemon(True)
-        worker.start()
+for i in xrange(AVAILABLE_THREADS):
+    worker = Thread(target=scanner_function, args=(i, ipNetworksQueue))
+    worker.setDaemon(True)
+    worker.start()
 
-    ipNetworksQueue.join()
-    print "Program finished"
+ipNetworksQueue.join()
+print "Program finished"
 
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
