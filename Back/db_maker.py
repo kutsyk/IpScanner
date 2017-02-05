c75953914b31a306a40644217ce2ee558b342395
kutsyk <vasya.kutsyk@gmail.com>
2017-02-05 11:24:40 +0200

    dec added

--- Back/db_maker.py
+++ Back/db_maker.py
@@ -1,26 +1,36 @@
 from MyCloud import *
 import json
+import netaddr
 
 mongoClient = MongoConnector('local')
 ipDB = mongoClient.ipStats
 ipDevCollection = ipDB.ips_banners
 TCP = "tcp"
 
+def isNone(obj):
+    return banner["status"] is None
+
 def ProcessBanner(banner):
     info = banner["info"]
     banObj = json.loads(info)
 
-    banner["status"] = banObj["status"]
-    banner["ip"] = banObj["addresses"]["ipv4"]
-    banner["hostnames"] = banObj["hostnames"]
+    if not ("status" in banner.keys()):
+        banner["status"] = banObj["status"]
+    if not ("ip" in banner.keys()):
+        banner["ip"] = banObj["addresses"]["ipv4"]
+    if not ("dec_ip" in banner.keys()):
+        banner["dec_ip"] = int(netaddr.IPAddress(banObj["addresses"]["ipv4"]))
+    if not ("hostnames" in banner.keys()):
+        banner["hostnames"] = banObj["hostnames"]
 
     if TCP in banObj.keys():
-        banner["ports"] = []
-        for item in banObj["tcp"]:
-            banner["ports"].append(item)
+        if not ("ports" in banner.keys()):
+            banner["ports"] = []
+            for item in banObj["tcp"]:
+                banner["ports"].append(item)
 
-        for port in banner["ports"]:
-            banner[str(port)] = banObj["tcp"][str(port)]
+            for port in banner["ports"]:
+                banner[str(port)] = banObj["tcp"][str(port)]
     # tcp is null
     return banner
 
